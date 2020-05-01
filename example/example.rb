d1f10d40b9edca595233350dbaf788c5ba33839a
Landon Schropp <schroppl@gmail.com>
2020-05-01 11:48:31 -0700

    Add an example file for Rubocop
    Rubocop wasn't running all of our cops with only the Gemfile and
    gemspec. Adding an example file ensures all of the cops run without
    warnings or errors.

---
+++ example/example.rb
@@ -0,0 +1,5 @@
+# Rubocop ignores some rules when it only lints the Gemfile and gemspec files. This file serves as
+# an example of a Ruby file so we can ensure all of the linter rules run without warnings or errors.
+module Example
+  def example; end
+end
